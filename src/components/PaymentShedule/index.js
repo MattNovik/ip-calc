af603bdb95d99c6b0c3aed7e7382e29503028f38
mattNov <matvej.novik@gmail.com>
2022-06-03 11:57:19 +0300

    fix shedule

--- src/components/PaymentShedule/index.js
+++ src/components/PaymentShedule/index.js
@@ -14,27 +14,29 @@ const PaymentShedule = (data) => {
   return (
     <div className='payment-shedule' id='payment-shedule'>
       <h2>График платежей по ипотеке</h2>
-      <ResponsiveContainer width='100%' height='100%'>
-        <AreaChart
-            width={500}
-            height={400}
-            data={sheduleData.data}
-            margin={{
-              top: 10,
-              right: 30,
-              left: 0,
-              bottom: 0,
-            }}
-          >
-            <CartesianGrid strokeDasharray='3 3' />
-            <XAxis dataKey='name' />
-            <YAxis width={longestLabelLength * 10} tickFormatter={tick => {return tick.toLocaleString()}} />
-            <Legend iconType='circle'/>
-            <Tooltip formatter={(value) => new Intl.NumberFormat().format(value)}/>
-            <Area type='monotone' name='Погашение долга' dataKey='rub' stackId='1' stroke='#8884d8' fill='#8884d8' unit=' руб.'/>
-            <Area type='monotone' name='Погашение процентов' dataKey='rubDop' stackId='1' stroke='#82ca9d' fill='#82ca9d' unit=' руб.'/>
-          </AreaChart>
+      <div className='payment-shedule__wrapper'>
+        <ResponsiveContainer width='100%' height='100%'>
+          <AreaChart
+              width={500}
+              height={400}
+              data={sheduleData.data}
+              margin={{
+                top: 10,
+                right: 30,
+                left: 0,
+                bottom: 0,
+              }}
+            >
+              <CartesianGrid strokeDasharray='3 3' />
+              <XAxis dataKey='name' />
+              <YAxis width={longestLabelLength * 10} tickFormatter={tick => {return tick.toLocaleString()}} />
+              <Legend iconType='circle'/>
+              <Tooltip formatter={(value) => new Intl.NumberFormat().format(value)}/>
+              <Area type='monotone' name='Погашение долга' dataKey='rub' stackId='1' stroke='#8884d8' fill='#8884d8' unit=' руб.'/>
+              <Area type='monotone' name='Погашение процентов' dataKey='rubDop' stackId='1' stroke='#82ca9d' fill='#82ca9d' unit=' руб.'/>
+            </AreaChart>
         </ResponsiveContainer>
+      </div>
     </div>
   )
 }
