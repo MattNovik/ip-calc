e6d30a9c5ca577eaff99aa1dc22d248d7c208410
mattNov <matvej.novik@gmail.com>
2022-06-06 13:22:48 +0300

    fix styles

--- src/components/PaymentShedule/index.js
+++ src/components/PaymentShedule/index.js
@@ -9,7 +9,7 @@ const PaymentShedule = (data) => {
     setSheduleData(data);
   })
 
-  const longestLabelLength = (data.data.map(c => c.rub.toLocaleString()).reduce((acc, cur) => (cur.length > acc ? cur.length : acc), 0));
+  const longestLabelLength = (data.data.map(c => c.rub.toLocaleString()).reduce((acc, cur) => (cur.length > acc ? cur.length : acc), 0)) + 2;
 
   return (
     <div className='payment-shedule' id='payment-shedule'>
@@ -29,8 +29,8 @@ const PaymentShedule = (data) => {
             >
               <CartesianGrid strokeDasharray='3 3' />
               <XAxis dataKey='name' />
-              <YAxis width={longestLabelLength * 10} tickFormatter={tick => {return tick.toLocaleString()}} />
-              <Legend iconType='circle'/>
+              <YAxis width={window.screen.width >= 1400 ? longestLabelLength*10/1400*window.screen.width : longestLabelLength*10} tickFormatter={tick => {return tick.toLocaleString()}} />
+              <Legend iconType='circle' width='100%'/>
               <Tooltip formatter={(value) => new Intl.NumberFormat().format(value)}/>
               <Area type='monotone' name='Погашение долга' dataKey='rub' stackId='1' stroke='#8884d8' fill='#8884d8' unit=' руб.'/>
               <Area type='monotone' name='Погашение процентов' dataKey='rubDop' stackId='1' stroke='#82ca9d' fill='#82ca9d' unit=' руб.'/>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,4 @@
-export const getCookie = key =>
+/* export const getCookie = key =>
 document.cookie.split("; ").reduce((total, currentCookie) => {
   const item = currentCookie.split("=");
   const storedKey = item[0];
@@ -6,7 +6,7 @@ document.cookie.split("; ").reduce((total, currentCookie) => {
   return key === storedKey 
     ? decodeURIComponent(storedValue) 
     : total;
-},'');
+},''); */
 
 export const handleValidateNumbers = (e) => {
   let newValueInput = e.target.value.replace(/^0+/, '').replace(/[^\d]/g, '').replace(/\s+/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, " ");
